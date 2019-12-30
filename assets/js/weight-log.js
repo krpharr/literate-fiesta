eaba43ea45b3e9b0f45eca97adc24e478e44483f
rpharr <rpharr@richmond.edu>
2019-12-30 14:29:09 -0500

    added some responsivness and weight-log.html to view past entries

---
+++ assets/js/weight-log.js
@@ -0,0 +1,18 @@
+ls = JSON.parse(localStorage.getItem("lf-weight"));
+
+if (ls === null || ls.length < 1) {
+    // deal with no entires
+} else {
+    ls.forEach(element => {
+        console.log(element);
+        if (element.type === "weight") {
+            let container = $("<div>");
+            // container.attr("style", "display:flex; justify-content: space-between;");
+            let timestamp = moment(element.timestamp).format("MM/DD/YYYY hh:mm a");
+            let span1 = $("<span>").attr("style", "padding:4px 8px;").text(`${element.weight}`);
+            let span2 = $("<span>").attr("style", "padding:4px 8px;").text(`${timestamp}`);
+            container.append(span1, span2);
+            $("#weight-log-ID").append(container);
+        }
+    });
+}

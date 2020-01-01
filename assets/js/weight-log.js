0e226b54b9e89c17d4a2ae1badaa36b370a8ba54
rpharr <rpharr@richmond.edu>
2020-01-01 12:12:54 -0500

    updated weight-log.html to display dressed boolean and edited menu

--- assets/js/weight-log.js
+++ assets/js/weight-log.js
@@ -10,8 +10,11 @@ if (ls === null || ls.length < 1) {
             // container.attr("style", "display:flex; justify-content: space-between;");
             let timestamp = moment(element.timestamp).format("MM/DD/YYYY hh:mm a");
             let span1 = $("<span>").attr("style", "padding:4px 8px;").text(`${element.weight}`);
-            let span2 = $("<span>").attr("style", "padding:4px 8px;").text(`${timestamp}`);
-            container.append(span1, span2);
+            var str;
+            element.dressed ? str = "dressed" : str = "";
+            let span2 = $("<span>").attr("style", "padding:4px 8px;").text(str);
+            let span3 = $("<span>").attr("style", "padding:4px 8px;").text(`${timestamp}`);
+            container.append(span1, span2, span3);
             $("#weight-log-ID").append(container);
         }
     });
